8eee46ecad83c48395fc41c466c1490fe0bb3dd3
Taj <taj.poulsen@>
2023-10-28 14:30:48 -0600

    local storage email and name

--- startup_html/contact.js
+++ startup_html/contact.js
@@ -2,12 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const contactForm = document.getElementById('contactForm');
     const firstNameInput = document.getElementById('firstName');
     const lastNameInput = document.getElementById('lastName');
+    const emailInput = document.getElementById('email');
     const userNameDisplay = document.getElementById('userNameDisplay'); // Element to display the user's name
 
     // Read and display the user name
     const userName = localStorage.getItem('userName');
     if (userName) {
         userNameDisplay.textContent = userName; // Display the user's name if it exists in localStorage
+    }
+
+    // Read and populate the user email
+    const userEmail = localStorage.getItem('userEmail');
+    if (userEmail) {
+        emailInput.value = userEmail; // Populate the email field if it exists in localStorage
     }
 
     // Handle form submission
@@ -17,6 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
         // Get user input
         const firstName = firstNameInput.value;
         const lastName = lastNameInput.value;
+        const email = emailInput.value;
 
         // Construct the user's full name
         const fullName = `${firstName} ${lastName}`;
@@ -24,9 +32,11 @@ document.addEventListener('DOMContentLoaded', function () {
         // Display the user's full name
         userNameDisplay.textContent = fullName;
 
-        // Save the user's name in localStorage
+        // Save the user's name and email in localStorage
         localStorage.setItem('userName', fullName);
+        localStorage.setItem('userEmail', email);
     });
 });
+
 
 
