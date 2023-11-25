94b43cf03a57233e80d724cbd106e55a35f1a405
Taj <taj.poulsen@>
2023-11-24 17:48:04 -0700

    security and login

--- startup_html/database.js
+++ startup_html/database.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('orders');
+const userCollection = db.collection('user');
 const ordersCollection = db.collection('submission');
 
 async function connectToDatabase() {
@@ -14,6 +17,37 @@ async function connectToDatabase() {
     console.error(`Unable to connect to the database: ${error.message}`);
     throw error;
   }
+}
+
+// This will asynchronously test the connection and exit the process if it fails
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
 }
 
 async function addOrder(order) {
@@ -58,4 +92,7 @@ module.exports = {
   connectToDatabase,
   addOrder,
   getOrders,
+  getUser,
+  getUserByToken,
+  createUser,
 };
--- startup_html/public/login.js
+++ startup_html/public/login.js
@@ -12,7 +12,7 @@ function isElementInViewport(el) {
 // Function to handle scroll event
 function onScroll() {
     const welcomeHeader = document.querySelector('.welcome-header');
-    
+
     if (isElementInViewport(welcomeHeader)) {
         welcomeHeader.style.opacity = 1;
         window.removeEventListener('scroll', onScroll); // Remove the scroll event listener after animation
@@ -57,3 +57,85 @@ function displayQuote() {
 if (isElementInViewport(document.querySelector('.welcome-header'))) {
     displayQuote();
 }
+
+// Check if the DOM content is loaded before executing the script
+document.addEventListener('DOMContentLoaded', async () => {
+    const userName = localStorage.getItem('userName');
+    const loginControls = document.getElementById('loginControls');
+    const playControls = document.getElementById('playControls');
+
+    if (userName) {
+        document.getElementById('customerName').textContent = userName;
+        loginControls.style.display = 'none';
+        playControls.style.display = 'block';
+    } else {
+        loginControls.style.display = 'block';
+        playControls.style.display = 'none';
+
+        // Check if the current page is one of the restricted pages
+        const restrictedPages = ['checkout.html', 'contact.html', 'faq.html', 'inventory.html'];
+        const currentLocation = window.location.pathname.split('/').pop(); // Get the current page name
+
+        if (restrictedPages.includes(currentLocation)) {
+            // Redirect to the home page for non-logged-in users
+            window.location.href = 'index.html';
+        }
+    }
+});
+
+async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+
+    if (response.ok) {
+        localStorage.setItem('userName', userName);
+        document.querySelector('#customerName').textContent = userName;
+        setDisplay('loginControls', 'none');
+        setDisplay('playControls', 'block');
+    } else {
+        const body = await response.json();
+        const modalEl = document.querySelector('#msgModal');
+        modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+        const msgModal = new bootstrap.Modal(modalEl, {});
+        msgModal.show();
+    }
+}
+
+function order() {
+    // Assuming order() is a placeholder for an action when the user clicks an "Order" button
+    // You can replace it with the actual functionality you want.
+}
+
+function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+        method: 'delete',
+    }).then(() => (window.location.href = '/'));
+}
+
+// Other functions remain unchanged
+
+function setDisplay(controlId, display) {
+    const playControlEl = document.querySelector(`#${controlId}`);
+    if (playControlEl) {
+        playControlEl.style.display = display;
+    }
+}
+
+
+  
