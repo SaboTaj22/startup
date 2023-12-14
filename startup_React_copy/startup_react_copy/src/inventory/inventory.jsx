1fade9d4db8de0838eeffee964a514fd5a06019f
Taj <taj.poulsen@>
2023-12-14 13:55:56 -0700

    Created components

---
+++ startup_React_copy/startup_react_copy/src/inventory/inventory.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function inventory() {
+  return (
+    <header className="welcome-header">
+      <h1>Welcome</h1>
+      <p>Subscribe to receive updates!</p>
+      {/* Add any other content specific to WelcomeHeader */}
+    </header>
+  );
+}
+
+export default inventory;
